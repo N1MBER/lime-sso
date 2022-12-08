4ba5ed137c1d2e46737620e840b1e6e0fc62cdaa
N1MBER <socialfox33@gmail.com>
2022-12-08 10:16:35 +0300

    chore(Builder): add builder, linter, prettier and other tools for project

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'all',
+  arrowParens: 'always',
+  jsxBracketSameLine: false,
+  semi: true,
+  endOfLine: 'auto',
+  tabWidth: 2,
+  printWidth: 80,
+};
---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+module.exports = {
+  entry: {
+    main: ['@babel/polyfill', path.resolve(__dirname, './src/index')],
+  },
+  devtool: 'source-map',
+  mode,
+  output: {
+    publicPath: '/',
+  },
+  devServer: {
+    open: true,
+    port: 3000,
+    contentBase: ['./src', './public'],
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    plugins: [new TsconfigPathsPlugin({ baseUrl: 'src' })],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: '/node_modules/',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+              ],
+              plugins: ['@babel/plugin-transform-runtime'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        exclude: '/node_modules/',
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.json$/,
+        type: 'javascript/auto',
+        use: ['json-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv({
+      path: './.env.development',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.svg',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/assets/images',
+          to: 'images',
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+};
