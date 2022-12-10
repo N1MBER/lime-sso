0e022498adf76c2e0938e73ccc96f54db7b0aa4c
N1MBER <socialfox33@gmail.com>
2022-12-10 21:28:12 +0300

    fix(Minor): minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { Flex } from './components/Layout/Flex';
 import { GradientContainer } from './components/Layout/GradientContainer';
 import { ThemeProvider } from './components/ThemeProvider';
 import { ctx } from '##/atoms/app';
+import { Login } from './pages/Login/Login';
 
 export const App = () => {
   return (
     <reatomContext.Provider value={ctx}>
       <ThemeProvider>
         <GradientContainer>
-          <Flex>Kek</Flex>
+          <Login />
         </GradientContainer>
       </ThemeProvider>
     </reatomContext.Provider>
