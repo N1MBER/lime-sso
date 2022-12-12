af5ed14bc461bd686495868ce6673ab6505368c5
artalar <artalar@ya.ru>
2022-12-12 05:20:28 +0000

    refactor(Theme): use reatomRecord

--- src/atoms/theme/index.tsx
+++ src/atoms/theme/index.tsx
@@ -2,7 +2,8 @@ import { IconComponent } from '@consta/uikit/Icon';
 import { IconMoon } from '@consta/uikit/IconMoon';
 import { IconSun } from '@consta/uikit/IconSun';
 import { ThemePreset } from '@consta/uikit/Theme';
-import { action, atom } from '@reatom/core';
+import { atom } from '@reatom/core';
+import { reatomRecord } from '@reatom/primitives';
 import { onUpdate } from '@reatom/hooks';
 import { presetSsoDark, presetSsoLight, ThemeName } from '##/assets/themes';
 
@@ -24,31 +25,15 @@ const snapTheme = themes.find(
   (item) => item.color.primary === localStorage.getItem(KEY),
 );
 
-export const themeAtom = atom<ThemePreset>(snapTheme || presetSsoLight);
+export const themeAtom = atom<ThemePreset>(
+  snapTheme || presetSsoLight,
+  'themeAtom',
+);
 
 onUpdate(themeAtom, (ctx, value) =>
-  localStorage.setItem(KEY, value.color.primary),
+  ctx.schedule(() => localStorage.setItem(KEY, value.color.primary)),
 );
 
-export const htmlModsAtom = atom<Record<string, string | boolean | undefined>>(
-  {},
-);
-
-export const htmlModsActionAdd = action(
-  (ctx, payload: { name: string; value: string | boolean | undefined }) => {
-    htmlModsAtom(ctx, {
-      ...ctx.get(htmlModsAtom),
-      [payload.name]: payload.value,
-    });
-  },
-);
-
-export const htmlModsActionDel = action((ctx, name: string) => {
-  const state = ctx.get(htmlModsAtom);
-  if (name in state) {
-    const newState = { ...state };
-    delete newState[name];
-
-    htmlModsAtom(ctx, newState);
-  }
-});
+export const htmlModsAtom = reatomRecord<
+  Record<string, string | boolean | undefined>
+>({}, 'htmlModsAtom');
